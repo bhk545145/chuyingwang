5dc891ceb16686ee716ec79ed075e3a0fb696410
bhk545145 <bhk545145@163.com>
2016-05-28 22:12:49 +0800

    卡片式效果

--- chuyingwang/ViewController.m
+++ chuyingwang/ViewController.m
@@ -64,46 +64,27 @@
               index:(NSUInteger)index
               views:(NSArray<UIView *> *)views
       swipeableView:(ZLSwipeableView *)swipeableView {
-    CGFloat degree = sin(0 * index);
+    NSUInteger  i = index;
     NSTimeInterval duration = 0.4;
-    CGPoint offset = CGPointMake(0, CGRectGetHeight(swipeableView.bounds) * 0.3);
-    CGPoint translation = CGPointMake(0, index * 15.0);
     [self rotateAndTranslateView:view
-                       forDegree:degree
-                     translation:translation
+                       forDegree:i
                         duration:duration
-              atOffsetFromCenter:offset
                    swipeableView:swipeableView];
 }
 
-- (CGFloat)degreesToRadians:(CGFloat)degrees {
-    return degrees * M_PI / 180;
-}
-
-- (CGFloat)radiansToDegrees:(CGFloat)radians {
-    return radians * 180 / M_PI;
-}
 
 - (void)rotateAndTranslateView:(UIView *)view
-                     forDegree:(float)degree
-                   translation:(CGPoint)translation
+                     forDegree:(NSUInteger)i
                       duration:(NSTimeInterval)duration
-            atOffsetFromCenter:(CGPoint)offset
                  swipeableView:(ZLSwipeableView *)swipeableView {
-    float rotationRadian = [self degreesToRadians:degree];
     [UIView animateWithDuration:duration
                           delay:0
                         options:UIViewAnimationOptionAllowUserInteraction
                      animations:^{
-                         view.center = [swipeableView convertPoint:swipeableView.center
-                                                          fromView:swipeableView.superview];
-                         CGAffineTransform transform =
-                         CGAffineTransformMakeTranslation(0, offset.y);
-                         transform = CGAffineTransformRotate(transform, rotationRadian);
-                         transform = CGAffineTransformTranslate(transform, -offset.x, -offset.y);
-                         transform =
-                         CGAffineTransformTranslate(transform, translation.x, translation.y);
+                         view.center = [swipeableView convertPoint:swipeableView.center fromView:swipeableView.superview];
+                         CGAffineTransform transform = CGAffineTransformMake(1 - i*0.06, 0, 0,1- i*0.06, 0, i*25);
                          view.transform = transform;
+                         view.alpha = 1 - i * 0.2;
                      }
                      completion:nil];
 }
