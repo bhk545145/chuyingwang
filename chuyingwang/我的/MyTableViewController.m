287c223a59d38dc115e5eccfc2d6fedfd4988cab
bhk545145 <bhk545145@163.com>
2016-06-02 20:25:06 +0800

    my

---
+++ chuyingwang/我的/MyTableViewController.m
@@ -0,0 +1,75 @@
+//
+//  MyTableViewController.m
+//  chuyingwang
+//
+//  Created by 白洪坤 on 16/6/2.
+//  Copyright © 2016年 白洪坤. All rights reserved.
+//
+
+#import "MyTableViewController.h"
+
+@interface MyTableViewController ()
+
+@end
+
+@implementation MyTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 2;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *SimleTableIdentifier;
+    if (indexPath.section == 0) {
+        if(indexPath.row == 0){
+            SimleTableIdentifier =@"myfirstcell";
+        }else if (indexPath.row == 1){
+            SimleTableIdentifier =@"mysecondcell";
+        }else if (indexPath.row == 2){
+            SimleTableIdentifier =@"mythirdcell";
+        }
+    }else if (indexPath.section == 1){
+        
+    }
+    
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SimleTableIdentifier];
+    
+    return cell;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    static int height;
+    if (indexPath.section == 0) {
+        if(indexPath.row == 0){
+            height = 183;
+        }else if (indexPath.row == 1){
+            height = 44;
+        }else if (indexPath.row == 2){
+            height = 44;
+        }
+    }else if (indexPath.section == 1){
+        
+    }
+    
+    return height;
+}
+
+@end
