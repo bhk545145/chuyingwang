3b79d189018f276f454ed685901e797eb2602881
bhk545145 <bhk545145@163.com>
2016-06-04 01:10:36 +0800

    123

--- chuyingwang/CardView.m
+++ chuyingwang/CardView.m
@@ -7,6 +7,7 @@
 //
 
 #import "CardView.h"
+#import "bhkCommon.h"
 
 @interface CardView (){
     //项目图片
@@ -18,7 +19,7 @@
     //融资进度
     UILabel *financingStatuslab;
     //项目介绍
-    UILabel *projectIntroductionlab;
+    UITextView *projectIntroductionlab;
     //阅读和收藏
     UILabel *readingCollection;
     //融资进度表
@@ -45,14 +46,29 @@
         [self.ProjectCard addSubview:projectbtn];
         
         projectNamebtn = [[UIButton alloc]initWithFrame:CGRectMake(0, projectbtn.bounds.size.height - 40, projectbtn.bounds.size.width, 40)];
+        projectNamebtn.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:13.0];
+        [projectNamebtn setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [projectbtn addSubview:projectNamebtn];
         
-        financingStatusbtn = [[UIButton alloc]initWithFrame:CGRectMake(100, 250, 70, 22)];
+        financingStatusbtn = [[UIButton alloc]initWithFrame:CGRectMake(90, 260, 70, 22)];
+        [financingStatusbtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        financingStatusbtn.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:10.0];
         [self.ProjectCard addSubview:financingStatusbtn];
         
-        financingStatuslab = [[UILabel alloc]initWithFrame:CGRectMake(100, 300, 100, 22)];
+        financingStatuslab = [[UILabel alloc]initWithFrame:CGRectMake(100, 285, 100, 22)];
+        financingStatuslab.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:10.0];
         [self.ProjectCard addSubview:financingStatuslab];
-         
+        
+        projectIntroductionlab = [[UITextView alloc]initWithFrame:CGRectMake(25, 350,self.ProjectCard.bounds.size.width - 50, 40)];
+        projectIntroductionlab.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:11.0];
+        projectIntroductionlab.textColor = IWcolor(135, 135, 135);
+        projectIntroductionlab.backgroundColor = IWcolor(244, 244, 244);
+        [self.ProjectCard addSubview:projectIntroductionlab];
+        
+        readingCollection = [[UILabel alloc]initWithFrame:CGRectMake(20, 400, self.ProjectCard.bounds.size.width - 50, 40)];
+        readingCollection.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:11.0];
+        readingCollection.textColor = IWcolor(83, 83, 83);
+        [self.ProjectCard addSubview:readingCollection];
         [self setup];
     }
     return self;
@@ -69,16 +85,19 @@
     self.layer.cornerRadius = 10.0;
     
     [projectbtn setBackgroundImage:iCardimg forState:UIControlStateNormal];
+    
     [projectNamebtn setBackgroundImage:[UIImage imageNamed:@"black_pic"] forState:UIControlStateNormal];
     [projectNamebtn setTitle:Cardstr forState:UIControlStateNormal];
-    projectNamebtn.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:13.0];
-    [projectNamebtn setTitleShadowColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    
     
     [financingStatusbtn setBackgroundImage:[UIImage imageNamed:@"financing_status"] forState:UIControlStateNormal];
     [financingStatusbtn setTitle:@"股权融资中" forState:UIControlStateNormal];
-    [financingStatusbtn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-    financingStatusbtn.titleLabel.font = [UIFont fontWithName:@"Arial Rounded MT Bold" size:10.0];
     
+    financingStatuslab.text = @"已完成25%";
+    
+    projectIntroductionlab.text = @"该项目由阿里系知名创业团队带领，团队协作力度和执行度都很优秀";
+    
+    readingCollection.text = [NSString stringWithFormat:@"阅读(%d)收藏(%d)",5,8];
 }
 
 @end
